9870c1f84c00e5af30ec34ead42dc4e1f55258d1
asanjinez <asanjinez@hexacta.com>
2022-05-19 11:53:22 -0300

    elimino carpeta equivocada

--- src/main/java/com/iii/exceptions/NombreExistenteException.java
+++
@@ -1,7 +0,0 @@
-package com.iii.exceptions;
-
-public class NombreExistenteException extends RuntimeException{
-    public NombreExistenteException(String nombre){
-        super("Ya existe el nombre " + nombre);
-    }
-}
